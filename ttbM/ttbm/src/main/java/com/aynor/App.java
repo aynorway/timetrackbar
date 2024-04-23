e9ef2448d4a68eb4a1eecc23007a261c5a16be8e
aynorway <adrianynorway@gmail.com>
2024-04-23 16:58:50 -0400

    Initial commit new maven

---
+++ ttbM/ttbm/src/main/java/com/aynor/App.java
@@ -0,0 +1,13 @@
+package com.aynor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ttbM/ttbm/src/test/java/com/aynor/AppTest.java
@@ -0,0 +1,20 @@
+package com.aynor;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
