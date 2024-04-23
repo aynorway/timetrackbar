f24b3854244f642137e4ddb69e1e70d9c8effca9
aynorway <adrianynorway@gmail.com>
2024-04-23 17:27:31 -0400

    Initial commit new maven

--- ttbM/ttbm/src/main/java/com/aynor/App.java
+++ ttbM/ttbm/src/main/java/com/aynor/App.java
@@ -1,13 +1,13 @@
 package com.aynor;
 
 /**
- * Hello world!
+ * Hello ttbm!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "—————————————— \n Hello ttbm! \n——————————————" );
     }
 }
