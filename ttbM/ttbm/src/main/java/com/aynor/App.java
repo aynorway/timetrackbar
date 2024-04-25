9ca0277cd97fdf99034ccc458b32be47fa981dca
aynorway <adrianynorway@gmail.com>
2024-04-25 16:20:40 -0400

    一个可用的初始的maven App.java模板

--- ttbM/ttbm/src/main/java/com/aynor/App.java
+++ ttbM/ttbm/src/main/java/com/aynor/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "—————————————— \n Hello ttbm! \n——————————————" );
+        System.out.println( "—————————————— \n Hello ttbm 模板! \n——————————————" );
     }
 }
