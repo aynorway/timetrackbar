d943f7c6c3c6c58748fea2648d051a416d6537ca
aynorway <adrianynorway@gmail.com>
2024-04-28 01:08:37 -0400

    增加CommonMenuSetup.java内容

--- ttbM/ttbm/src/main/java/com/aynor/menu/MacOSMenuSetup.java
+++ ttbM/ttbm/src/main/java/com/aynor/menu/MacOSMenuSetup.java
@@ -7,11 +7,17 @@ import javax.swing.JMenu;
 public class MacOSMenuSetup implements MenuSetup {
     @Override
     public void setupMenu(JFrame mainFrame) {
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        menuBar.add(fileMenu);
-        mainFrame.setJMenuBar(menuBar);
-        
+        // JMenuBar menuBar = new JMenuBar();
+        // JMenu fileMenu = new JMenu("File");
+        // menuBar.add(fileMenu);
+        // mainFrame.setJMenuBar(menuBar);
+
+        System.out.println("---\n Operating System: " + System.getProperty("os.name") + "\n---");
+
+        // 使用 CommonMenuSetup 类来设置菜单
+        CommonMenuSetup.setupCommonMenu(mainFrame);
+
         System.out.println("---\n MacOSMenuSetup 已加载 \n---");
+
     }
 }
--- ttbM/ttbm/src/main/java/com/aynor/ui/MainFrame.java
+++ ttbM/ttbm/src/main/java/com/aynor/ui/MainFrame.java
@@ -24,6 +24,7 @@ public class MainFrame {
     private void setupMainFrame() {
         mainFrame = new JFrame("TimeTrackBar Application");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
         mainFrame.setSize(900, 70); // 设置窗口大小
         mainFrame.setLocationRelativeTo(null); // 居中显示
     }
@@ -35,8 +36,21 @@ public class MainFrame {
     }
 
     private void setupMenu() {
-        MenuSetup menuSetup = System.getProperty("os.name").toLowerCase().contains("mac") ?
-                              new MacOSMenuSetup() : new WindowsMenuSetup();
+        // MenuSetup menuSetup = System.getProperty("os.name").toLowerCase().contains("mac") ?
+        //                       new MacOSMenuSetup() : new WindowsMenuSetup();
+        // menuSetup.setupMenu(mainFrame);
+
+        MenuSetup menuSetup;
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("mac")) {
+            menuSetup = new MacOSMenuSetup();
+        } else if (os.contains("win")) {
+            menuSetup = new WindowsMenuSetup();
+        } else {
+            // 对于其他未知操作系统，可能不提供特定的菜单设置
+            menuSetup = frame -> {
+            }; // No-op
+        }
         menuSetup.setupMenu(mainFrame);
     }
 
