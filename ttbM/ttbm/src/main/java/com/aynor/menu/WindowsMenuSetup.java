8e44c8e649c77b4ed7a98c59292beecb4a93236f
aynorway <adrianynorway@gmail.com>
2024-04-29 00:07:05 -0400

    略

--- ttbM/ttbm/src/main/java/com/aynor/menu/WindowsMenuSetup.java
+++ ttbM/ttbm/src/main/java/com/aynor/menu/WindowsMenuSetup.java
@@ -1,16 +1,10 @@
 package com.aynor.menu;
 
 import javax.swing.JFrame;
-// import javax.swing.JMenuBar;
-// import javax.swing.JMenu;
 
 public class WindowsMenuSetup implements MenuSetup {
     @Override
     public void setupMenu(JFrame mainFrame) {
-        // JMenuBar menuBar = new JMenuBar();
-        // JMenu fileMenu = new JMenu("File");
-        // menuBar.add(fileMenu);
-        // mainFrame.setJMenuBar(menuBar);
 
         System.out.println("---\n Operating System: \n " + System.getProperty("os.name") + "\n---");
 
