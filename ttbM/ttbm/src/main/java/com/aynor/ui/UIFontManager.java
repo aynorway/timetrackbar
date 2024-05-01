1756c531ba65db7ecf81f1cef44691251e474d00
aynorway <adrianynorway@gmail.com>
2024-05-01 13:56:41 -0400

    增加UIFontManager备注

--- ttbM/ttbm/src/main/java/com/aynor/ui/UIFontManager.java
+++ ttbM/ttbm/src/main/java/com/aynor/ui/UIFontManager.java
@@ -4,12 +4,16 @@ import javax.swing.UIManager;
 import javax.swing.plaf.FontUIResource;
 
 public class UIFontManager {
-        public static void setDefaultUIFont(FontUIResource f) {
+    public static void setDefaultUIFont(FontUIResource f) {
         java.util.Enumeration<Object> keys = UIManager.getDefaults().keys();
+        // 遍历一下所有的键
         while (keys.hasMoreElements()) {
+            // 获取下一个键
             Object key = keys.nextElement();
             Object value = UIManager.get(key);
+            // 检查是否是 FontUIResource 实例
             if (value instanceof FontUIResource)
+                // 如果是字体资源, 传入字体f, 替换原来字体资源
                 UIManager.put(key, f);
         }
     }
